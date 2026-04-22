c4a308f38f48ed1be973e2c74b1de5ed3dfae18d
DeMo900 <dodoadam893@gmail.com>
2026-04-22 10:55:22 +0200

    added show/hide password

--- book-heaven/src/components/Input.tsx
+++ book-heaven/src/components/Input.tsx
@@ -1,4 +1,4 @@
-
+import { useState } from "react";
 interface InputProps {
     labelName:string;
     labelValue:string;
@@ -8,14 +8,26 @@ interface InputProps {
 }
 
 const Input = ({labelName,labelValue,type,placeholder,onChange}:InputProps) => {
+    const [ishidden,setIsHidden] = useState(true);
+    const [isTypePassword,setIsTypePassword] = useState(type);
+    const handlePasswordVisibility = () => {
+        setIsHidden(!ishidden);
+        if(ishidden){
+            setIsTypePassword("text");
+        }else{
+            setIsTypePassword("password");
+        }
+    }
     return (
         <div className="ml-12 mt-4">
             <div className="flex items-center justify-between w-[90%]">
         <label className="block ml-4 mb-1 text-xs" htmlFor={labelName}>{labelValue}</label>
      {type === "password" && labelName === "password" && <p className="text-xs text-blue-950 cursor-pointer">Forgot Password?</p>}
          </div>
-        <input className="bg-stone-200 py-3 px-6 w-[95%] h-14 rounded-lg gap-2 mx-auto" type={type} placeholder={placeholder} onChange={onChange} />
-           
+         <div className="relative w-[95%] mx-auto">
+        <input className="bg-stone-200 py-3 px-6 w-[95%]  h-14 rounded-lg gap-2 mx-auto" type={isTypePassword} placeholder={placeholder} onChange={onChange} />
+        {type === "password" && labelName === "password" && <img className="text-xs text-blue-950 cursor-pointer absolute -translate-y-1/2 top-1/2 right-8 " onClick={handlePasswordVisibility} src={ishidden ? "hide.svg" : "show.svg"} alt="" />}
+        </div>
         </div>
     )
 }

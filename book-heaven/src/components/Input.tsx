6d23f888bd35f53d0d5d3cb7d7f9ce6c9405704a
DeMo900 <dodoadam893@gmail.com>
2026-04-26 22:35:24 +0300

    removing hard code

--- book-heaven/src/components/Input.tsx
+++ book-heaven/src/components/Input.tsx
@@ -6,35 +6,25 @@ interface InputProps {
     placeholder:string;
     showForgotPassword?:boolean;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    name:string;
 }
 
-const Input = ({labelName,labelValue,type,placeholder,showForgotPassword=false,onChange,name}:InputProps) => {
+const Input = ({labelName,labelValue,type,placeholder,showForgotPassword=false,onChange}:InputProps) => {
     const [ishidden,setIsHidden] = useState(true);
-    const [isTypePassword,setIsTypePassword] = useState(type);
-    const handlePasswordVisibility = () => {
-        setIsHidden(!ishidden);
-        if(ishidden){
-            setIsTypePassword("text");
-        }else{
-            setIsTypePassword("password");
-        }
-    }
     return (
     <>  
-        <div className="flex items-center justify-between w-[90%] md:w-full">  
-            <label className="block self-start mt-4 text-xs" htmlFor={labelName}>{labelValue}</label>
+        <div className="flex items-center  mt-4 justify-between w-[90%] md:w-full">  
+            <label className="block self-start text-xs" htmlFor={labelName}>{labelValue}</label>
             {type === "password" && labelName === "password" && showForgotPassword && 
-            <a href="http://localhost:5173/passwordreset">
+            <a href="/passwordreset">
                 <p className="text-xs text-blue-950 cursor-pointer hover:text-blue-700">Forgot Password?</p>
             </a>}
         </div>
         <div className="relative flex justify-center w-full">  
             <input className="bg-stone-200 py-3 px-6 w-[90%] md:w-full h-14 rounded-lg" 
-                type={isTypePassword} placeholder={placeholder} onChange={onChange} name={labelName}/>
+                type={ishidden ? type : "text"} placeholder={placeholder} onChange={onChange} name={labelName}/>
             {type === "password" && labelName === "password" && 
                 <img className="absolute -translate-y-1/2 top-1/2 right-8 cursor-pointer" 
-                    onClick={handlePasswordVisibility} 
+                    onClick={() => setIsHidden(!ishidden)} 
                     src={ishidden ? "hide.svg" : "show.svg"} alt="" />}
         </div>
     </>

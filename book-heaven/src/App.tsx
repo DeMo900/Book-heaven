43b485bb87e2a8a44523559ffbe337a6577912b0
DeMo900 <dodoadam893@gmail.com>
2026-04-27 21:41:36 +0300

    created the bottom navbar

--- book-heaven/src/App.tsx
+++ book-heaven/src/App.tsx
@@ -4,11 +4,13 @@ import SignupPage from "./pages/SignupPage"
 import ForgotPasswordPage from "./pages/ForgotPasswordPage"
 import ResetPassword from "./pages/ResetPassword"
 import Navbar from "./components/Navbar"
+import BottomNavBar from "./components/BottomNavBar"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navbar />} />
+    //  <Route path="/nav" element={<Navbar />} />
+      <Route path="/" element={<BottomNavBar />} />
 <Route path="/login" element={<LoginPage />} />
 <Route path="/signup" element={<SignupPage />} />
 <Route path="/passwordreset" element={<ForgotPasswordPage />} />
---
+++ book-heaven/src/components/BottomNavBar.tsx
@@ -0,0 +1,24 @@
+import {Library,Search,User,Star} from "lucide-react";
+const BottomNavBar = () => {
+    return (
+        <div className="flex bg-stone-200 fixed items-center justify-between bottom-2 w-full h-24 ">
+            <div className="flex flex-col justify-center items-center w-full hover:bg-stone-300 transition-colors duration-200">
+                <Library className="w-8 h-8 text-[#486459]" />
+                <p className="text-[#486459] text-2xl">Library</p>
+            </div>
+            <div className="flex flex-col justify-center items-center w-full hover:bg-stone-300 transition-colors duration-200">
+                <Search className="w-8 h-8 text-[#486459]" />
+                <p className="text-[#486459] text-2xl">Discover</p>
+            </div>
+            <div className="flex flex-col justify-center items-center w-full hover:bg-stone-300 transition-colors duration-200">
+                <User className="w-8 h-8 text-[#486459]" />
+                <p className="text-[#486459] text-2xl">Curated</p>
+            </div>
+            <div className="flex flex-col justify-center items-center w-full hover:bg-stone-300 transition-colors duration-200">
+                <Star className="w-8 h-8 text-[#486459]" />
+                <p className="text-[#486459] text-2xl">Profile</p>
+            </div>
+        </div>
+    )
+}
+export default BottomNavBar;

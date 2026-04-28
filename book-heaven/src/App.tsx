bde26ba4ce14e1c495aef0505ec950eb9446a83c
DeMo900 <dodoadam893@gmail.com>
2026-04-28 14:17:19 +0300

    added ul selecting depends on the page

--- book-heaven/src/App.tsx
+++ book-heaven/src/App.tsx
@@ -9,8 +9,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 const App = () => {
   return (
     <Routes>
-    //  <Route path="/nav" element={<Navbar />} />
-      <Route path="/" element={<BottomNavBar />} />
+   <Route path="/" element={<Navbar />} />
+      <Route path="/nav" element={<BottomNavBar />} />
 <Route path="/login" element={<LoginPage />} />
 <Route path="/signup" element={<SignupPage />} />
 <Route path="/passwordreset" element={<ForgotPasswordPage />} />
--- book-heaven/src/components/Navbar.tsx
+++ book-heaven/src/components/Navbar.tsx
@@ -1,13 +1,16 @@
 import Search from "./search";
 import { UserCircle } from "lucide-react";
+import {useLocation} from "react-router-dom";
 const Navbar = () => {
+    const location = useLocation();
+
     return (
         <div className="flex fixed p-10 items-center gap-8 w-full h-12 ">
 <h1 className="text-[#002542]  text-4xl shrink-0 font-bold font-serif font-stretch-500"> Book Heaven </h1>
 <ul className="hidden md:flex gap-2 md:gap-8">
-    <a href="/"><li className="text-[#486459] text-2xl font-medium hover:opacity-80 transition-opacity duration-200">Library</li></a>
-    <a href="/about"><li className="text-[#486459] text-2xl font-medium hover:opacity-80 transition-opacity duration-200">Discover</li></a>
-    <a href="/contact"><li className="text-[#486459] text-2xl font-medium hover:opacity-80 transition-opacity duration-200">Cruated</li></a>
+    <a href="/library"><li className={`${location.pathname === "/library" ? "text-[#002542] font-bold border-b-black border-b-2": "text-slate-500 "} text-xl font-medium hover:opacity-80 transition-opacity duration-200`}>Library</li></a>
+    <a href="/"><li className = {`${location.pathname === "/" ? "text-[#002542] font-bold border-b-black border-b-2": "text-slate-500 "} text-xl font-medium hover:opacity-80 transition-opacity duration-200`}>Discover</li></a>
+    <a href="/cruated"><li className={`${location.pathname === "/cruated" ? "text-[#002542]" : "text-slate-500"} text-xl font-medium hover:opacity-80 transition-opacity duration-200`}>Cruated</li></a>
 </ul>
 <Search />
 <UserCircle className="w-14 h-14 text-slate-700 hover:text-slate-500 transition-colors duration-200 hidden md:block" />

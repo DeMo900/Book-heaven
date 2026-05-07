4e59b0104b7ab3e7d122b4d6992a720d75edc660
DeMo900 <dodoadam893@gmail.com>
2026-05-07 12:15:58 +0300

    showing the trending book

--- book-heaven/src/pages/HomePage.tsx
+++ book-heaven/src/pages/HomePage.tsx
@@ -7,10 +7,12 @@ import {useDebounce} from "react-use";
 interface book {
     title :string,
     author : string,
+    desc : string
     coverurl : string
 }
 const HomePage = () => {
   const [booksArray, setBooksArray] = useState<book[]>([]); 
+  const [trendBook, setTrendBook] = useState<book | null>(null)
   const [staredBooksArray, setStaredBooksArray] = useState([]);
   const [isLoading,setLoading] = useState(false);
   const [searchValue,setSearchValue] = useState("");
@@ -22,12 +24,16 @@ useDebounce(() => setDebaunceValue(searchValue),600, [searchValue])
 const fetchBooks = await fetch("http://localhost:9000/books",{
     credentials:"include"
 })
-const response = await fetchBooks.json()
+const books= await fetchBooks.json()
 
-    setBooksArray(response.books)
-setStaredBooksArray(response.staredBooksArray)
+    setBooksArray(books.books)
+setStaredBooksArray(books.staredBooksArray)
 setLoading(false)
-
+const fetchTrendBook = await fetch("http://localhost:9000/trend-book",{
+    credentials:"include"
+})
+const trendBook = await fetchTrendBook.json()
+setTrendBook(trendBook.book)
     }catch(error){
         console.log(error)
     }
@@ -65,15 +71,15 @@ console.log(sum)
             <Navbar onChange={(e)=>setSearchValue(e.target.value)} />
             <div className=" md:flex flex flex-col-reverse  md:flex-row justify-between pt-32 pb-10 px-10   ">
 <div className="flex flex-col">
-<h1 className="text-left text-[#486459] mt-16 md:mt-0 font-stretch-90% font-bold text-xl">Featured Narrative</h1>
-<p className="text-[#002542] text-5xl leading-16 md:text-8xl font-[Playfair_Display] italic mt-4 font-medium md:mb-16">Whispers of the Ancient Willow</p>
-<p className="text-[#486459] font-[Inter] text-md md:text-2xl md:max-w-2xl md:leading-10 mb-8"> A profound journey through the forgotten landscapes of Northern Europe, exploring the intersection of folklore and the silent resilience of nature. </p>
+<h1 className="text-left text-[#486459] mt-16 md:mt-0 font-stretch-90% font-bold text-xl">Recomended</h1>
+<p className="text-[#002542] text-5xl leading-16 md:text-8xl font-[Playfair_Display] italic mt-4 font-medium md:mb-16">{trendBook?.title}</p>
+<p className="text-[#486459] font-[Inter] text-md md:text-2xl md:max-w-2xl md:leading-10 mb-8"> {trendBook?.desc} </p>
 <div className="flex gap-4 ">
     <button className="bg-[#002542] text-white px-8 py-4 rounded-xl text-lg hover:bg-slate-500 transition-colors duration-200">Read Now</button>
     <button className="bg-[#FBF9F5] text-[#002542] px-8 py-4 rounded-xl text-lg hover:bg-slate-500 transition-colors duration-200">Add to library</button>
 </div>
                 </div>
-                <img className="w-full md:w-[490px] border-l-4 border-l-amber-950 md:h-[110vh] h-[50vh] object-cover rounded-xl " src="https://lh3.googleusercontent.com/aida-public/AB6AXuBdvVtcv-t591bg0OLVbjDha1eNlEjuxP1RnrQ9Bz4yk3Apfi5Xkvb4W52yO2-ZkyCFzkf3w3d4YJQvv22TReXXP1fWYpHR71sdvr7_43vukeyyLBTO5REK4xaW2_IJcCOHoPDrp_NFSeQGTnZPBkSEx9_DxnvNFhaDrV1okf6NxOaJFBU1ntiYYMG9j-b5uHuVZ1FUJMtMiDOorINX-nr__hMez8BDCN8h4Hd8uoFet5SrSdAZSH92fWpQwo8uE1mHDEHCE83geeFd" alt="" />
+                <img className="w-full md:w-[490px] border-l-4 border-l-amber-950 md:h-[110vh] h-[50vh] object-cover rounded-xl " src={"http://localhost:9000/uploads/"+trendBook?.coverurl} alt="" />
                 </div>
                 <div className="flex flex-col gap-4 ml-4 ">
                     <h1 className="text-[#002542] font-[Playfair_Display] text-3xl">Explore Genres</h1>

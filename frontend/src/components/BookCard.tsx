a0c9c5ef47d674032a55164a298ffde074b7c4d4
DeMo900 <dodoadam893@gmail.com>
2026-08-30 22:24:23 +0300

    making the hard static

--- frontend/src/components/BookCard.tsx
+++ frontend/src/components/BookCard.tsx
@@ -30,10 +30,10 @@ const BookCard = ({
     setLiked(isLiked);
   }, [isLiked]);
   return (
-    <div className="flex flex-col w-[250px] group">
+    <div className="flex flex-col w-62.5 group">
       <div className="relative">
         <img
-          className="w-full h-[400px] object-cover rounded-xl "
+          className="w-full h-100 object-cover rounded-xl "
           src={image}
           onClick={handleClick}
           alt={title}
@@ -41,7 +41,7 @@ const BookCard = ({
         <div className="absolute top-2 left-[80%] opacity-45">
           <div
             onClick={handleLikeClick}
-            className="bg-white opacity-0 text-black h-10 w-8 p-1 py-2 rounded-lg group-hover:opacity-55 transition-opacity duration-800 cursor-pointer"
+            className="bg-white text-black h-10 w-8 p-1 py-2 rounded-lg  transition-opacity duration-800 cursor-pointer"
           >
             {liked ? <Heart className="fill-black" /> : <Heart />}
           </div>

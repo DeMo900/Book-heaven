a5c8dca56591da3c39b652a40fa8c9242a6b891d
DeMo900 <dodoadam893@gmail.com>
2026-08-31 15:18:55 +0300

    made the navbar smaller

--- frontend/src/components/BottomNavBar.tsx
+++ frontend/src/components/BottomNavBar.tsx
@@ -6,42 +6,42 @@ const BottomNavBar = () => {
   const location = useLocation();
 
   return (
-    <div className="flex bg-stone-200 fixed items-center md:hidden justify-between bottom-0 w-full h-24">
+    <div className="flex bg-stone-200 fixed items-center md:hidden justify-between bottom-0 w-full h-20 ">
       <Link
         to="/library"
         className="flex flex-col justify-center items-center w-full hover:bg-stone-300 transition-colors duration-200"
       >
         <Library
-          className={`w-8 h-8 ${location.pathname === "/library" ? "text-green-400/50" : "text-[#486459]"}`}
+          className={`w-6 h-6 ${location.pathname === "/library" ? "text-green-400/50" : "text-[#486459]"}`}
         />
-        <p className="text-[#486459] text-2xl">Library</p>
+        <p className="text-[#486459] text-xs">Library</p>
       </Link>
       <Link
         to="/"
         className="flex flex-col justify-center items-center w-full hover:bg-stone-300 transition-colors duration-200"
       >
         <Search
-          className={`w-8 h-8 ${location.pathname === "/" ? "text-green-400/50" : "text-[#486459]"}`}
+          className={`w-6 h-6 ${location.pathname === "/" ? "text-green-400/50" : "text-[#486459]"}`}
         />
-        <p className="text-[#486459] text-2xl">Discover</p>
+        <p className="text-[#486459] text-xs">Discover</p>
       </Link>
       <Link
         to="/profile"
         className="flex flex-col justify-center items-center w-full hover:bg-stone-300 transition-colors duration-200"
       >
         <User
-          className={`w-8 h-8 ${location.pathname === "/profile" ? "text-green-400/50" : "text-[#486459]"}`}
+          className={`w-6 h-6 ${location.pathname === "/profile" ? "text-green-400/50" : "text-[#486459]"}`}
         />
-        <p className="text-[#486459] text-2xl">Profile</p>
+        <p className="text-[#486459] text-xs">Profile</p>
       </Link>
       <Link
         to="/curated"
         className="flex flex-col justify-center items-center w-full hover:bg-stone-300 transition-colors duration-200"
       >
         <Star
-          className={`w-8 h-8 ${location.pathname === "/curated" ? "text-green-400/50" : "text-[#486459]"}`}
+          className={`w-6 h-6 ${location.pathname === "/curated" ? "text-green-400/50" : "text-[#486459]"}`}
         />
-        <p className="text-[#486459] text-2xl">Curated</p>
+        <p className="text-[#486459] text-xs">Curated</p>
       </Link>
     </div>
   );
